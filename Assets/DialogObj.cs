689d539c04917da62c83d0456341f524f008fc64
Esteban <36859022+Estebanmister@users.noreply.github.com>
2024-03-30 15:24:18 -0400

    dsadsad

---
+++ Assets/DialogObj.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialog")]
+public class DialogObj : ScriptableObject
+{
+    public string[] textLines;
+    public Sprite[] portraits;
+    public string[] names;
+    
+}
